7bde4fe3d3a4416790b672288509d607467ac8e5
milakaamiliaa <51574577+milakaamiliaa@users.noreply.github.com>
2019-11-30 10:38:27 +0700

    Feat/tutorial 8 react (#10)
    * finished feat 8 & starting to do readme
    
    * finished README.md

---
+++ frontend/src/components/List.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Item from "./Item";
+
+export default function List(props) {
+    const styles = {
+        heading : {
+            fontFamily: "courier new"
+        }
+    };
+    return(
+        <>
+        <h3 style={styles.heading}>{props.title}</h3>
+        <div className="list-group">
+            {props.items.map(item => (
+                <Item key={item.id} item={item} onChange={props.onItemClick} inputType={props.inputType}/>
+            ))}
+        </div>
+        </>
+    );
+}
